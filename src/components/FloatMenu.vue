4ee2bc0d8627fbea0beb1ccb25f8917ac9393609
Jinghao Hu <731409213@qq.com>
2026-08-04 04:30:27 +0800

    feat: add floating menu

---
+++ src/components/FloatMenu.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="float-menu">
+  <div>⌂</div>
+  <div>★</div>
+  <div>♡</div>
+  <div>☺</div>
+</div>
+</template>
+<style scoped>
+.float-menu{position:fixed;right:22px;top:50%;transform:translateY(-50%);display:flex;flex-direction:column;gap:12px}.float-menu div{width:48px;height:48px;background:white;border-radius:16px;box-shadow:0 5px 20px #ddd;display:flex;align-items:center;justify-content:center;color:#ff7895;font-size:22px}
+</style>
