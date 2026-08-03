24d23aa09f9c88460867eed767940e178f00c990
Jinghao Hu <731409213@qq.com>
2026-08-04 04:03:20 +0800

    feat: create homepage skeleton

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="page">
+    <h1>Together</h1>
+    <p>我们的故事，从这里开始。</p>
+  </main>
+</template>
+
+<style scoped>
+.page {
+  min-height: 100vh;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  flex-direction:column;
+}
+</style>
